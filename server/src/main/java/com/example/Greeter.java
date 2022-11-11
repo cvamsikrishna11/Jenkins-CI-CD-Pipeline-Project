4672e330f86b9d7c3cd0e03b35b58f7872dd71de
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2022-11-10 20:00:09 -0500

    testing sonar fail scenario

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,6 +16,9 @@ public class Greeter {
    * This is a method.
    */
   public final String greet(final String someone) {
+
+    String password = "Admin@12345";
+    System.out.println("Hello");
     
     return String.format("Hello Avinash, %s!", someone);
   }
